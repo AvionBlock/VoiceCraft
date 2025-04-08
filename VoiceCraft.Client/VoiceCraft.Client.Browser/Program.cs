96dd9b698dfd86a563066fba17875b7b2117ac77
Bonicgamer <bonicgamer@proton.me>
2025-04-08 19:45:59 -0400

    Initial work on Wasm/Browser
    This does not work!
    
    As of this commit the only thing that works is launching the program.
    Much of this is not properly tested, but hey, at least it's someting!
    
    Includes opus.a for testing purposes (Idk if it works).
    Includes simpleserv python script to serve on port 8000 (Also for
    testing purposes; Serves the output of dotnet publish).

--- VoiceCraft.Client/VoiceCraft.Client.Browser/Program.cs
+++ VoiceCraft.Client/VoiceCraft.Client.Browser/Program.cs
@@ -1,14 +1,44 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Browser;
+
+using VoiceCraft.Client;
+using VoiceCraft.Client.Services;
+using VoiceCraft.Client.Browser.Audio;
+using VoiceCraft.Client.Browser.Permissions;
+using VoiceCraft.Client.Browser;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.ApplicationModel;
+using Exception = System.Exception;
+
+// using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.InteropServices;
+
+using OpenTK.Audio.OpenAL;
 
 namespace VoiceCraft.Client.Browser
 {
     sealed class Program
     {
-        private static Task Main(string[] _) => BuildAvaloniaApp()
-            .WithInterFont()
-            .StartBrowserAppAsync("out");
+        private static Task Main(string[] _) {
+            OpenALLibraryNameContainer.OverridePath = "libal";
+            // OpenALLibraryNameContainer.OverridePath = "__Internal";
+            // OpenALLibraryNameContainer.OverridePath = "__Internal_emscripten";
+
+            Trace.Listeners.Add(new ConsoleTraceListener());
+
+            App.ServiceCollection.AddSingleton<AudioService, NativeAudioService>();
+            App.ServiceCollection.AddSingleton<BackgroundService, NativeBackgroundService>();
+            App.ServiceCollection.AddTransient<Microsoft.Maui.ApplicationModel.Permissions.Microphone, Microphone>();
+
+            return BuildAvaloniaApp()
+                .LogToTrace()
+                .WithInterFont()
+                .StartBrowserAppAsync("out");
+        }
 
         public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>();
     }
