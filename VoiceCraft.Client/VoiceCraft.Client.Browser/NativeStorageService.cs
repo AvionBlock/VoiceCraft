4f42e7379bb5eb3d6ba49b51000a7230dce9aae8
SineVector241 <flyingbirdasuka1@gmail.com>
2025-05-01 19:24:50 +1000

    Working storage.

--- VoiceCraft.Client/VoiceCraft.Client.Browser/NativeStorageService.cs
+++ VoiceCraft.Client/VoiceCraft.Client.Browser/NativeStorageService.cs
@@ -1,42 +1,29 @@
-using System;
-using System.IO;
 using System.Runtime.InteropServices.JavaScript;
 using System.Threading.Tasks;
 using VoiceCraft.Client.Services;
-using VoiceCraft.Core;
 
 namespace VoiceCraft.Client.Browser
 {
     public class NativeStorageService : StorageService
     {
-        private static readonly string ApplicationDirectory =
-            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Constants.ApplicationDirectory);
-
         public override bool Exists(string directory) =>
             JsNativeStorage.Exists(directory);
 
         public override byte[] Load(string directory) =>
             JsNativeStorage.Load(directory);
 
-        public override void Save(string directory, byte[] data)
+        public override void Save(string directory, byte[] data) =>
+            JsNativeStorage.Save(directory, data);
+
+        public override Task<byte[]> LoadAsync(string directory)
         {
-            CreateDirectoryIfNotExists();
-            File.WriteAllBytes(Path.Combine(ApplicationDirectory, directory), data);
+            return Task.FromResult(JsNativeStorage.Load(directory));
         }
 
-        public override async Task<byte[]> LoadAsync(string directory) =>
-            await File.ReadAllBytesAsync(Path.Combine(ApplicationDirectory, directory));
-
-        public override async Task SaveAsync(string directory, byte[] data)
+        public override Task SaveAsync(string directory, byte[] data)
         {
-            CreateDirectoryIfNotExists();
-            await File.WriteAllBytesAsync(Path.Combine(ApplicationDirectory, directory), data);
-        }
-
-        private static void CreateDirectoryIfNotExists()
-        {
-            if(!Directory.Exists(ApplicationDirectory))
-                Directory.CreateDirectory(ApplicationDirectory);
+            JsNativeStorage.Save(directory, data);
+            return Task.CompletedTask;
         }
     }
 
@@ -47,5 +34,8 @@ namespace VoiceCraft.Client.Browser
         
         [JSImport("load", "storage.js")]
         public static partial byte[] Load(string directory);
+        
+        [JSImport("save", "storage.js")]
+        public static partial void Save(string directory, byte[] data);
     }
 }
--- VoiceCraft.Client/VoiceCraft.Client.Browser/wwwroot/Exports/storage.js
+++ VoiceCraft.Client/VoiceCraft.Client.Browser/wwwroot/Exports/storage.js
@@ -1,11 +1,16 @@
 const textEncoder = new TextEncoder();
+const textDecoder = new TextDecoder();
 
 export function exists(directory) {
-    return window.localStorage.getItem(directory) !== undefined;
+    return window.localStorage.getItem(directory) !== undefined; //This is a bit stupid but it works.
 }
 
 export function load(directory) {
     const data = window.localStorage.getItem(directory);
     if (data === undefined) throw "No data available";
     return textEncoder.encode(data);
+}
+
+export function save(directory, data) {
+    window.localStorage.setItem(directory, textDecoder.decode(data));
 }
