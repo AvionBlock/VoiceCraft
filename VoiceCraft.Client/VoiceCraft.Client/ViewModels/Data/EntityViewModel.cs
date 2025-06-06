b66a5ba3acbb57a2d2a05235d1ffcf7f9c147fc5
SineVector241 <flyingbirdasuka1@gmail.com>
2025-06-06 19:42:58 +1000

    Begin on per entity volume.

--- VoiceCraft.Client/VoiceCraft.Client/ViewModels/Data/EntityViewModel.cs
+++ VoiceCraft.Client/VoiceCraft.Client/ViewModels/Data/EntityViewModel.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using VoiceCraft.Client.Network;
+using VoiceCraft.Core;
 
 namespace VoiceCraft.Client.ViewModels.Data;
 
@@ -11,8 +12,12 @@ public partial class EntityViewModel : ObservableObject
     [ObservableProperty] private bool _isVisible;
     [ObservableProperty] private float _volume;
 
+    private readonly VoiceCraftClientEntity _entity;
+    private bool _volumeUpdating;
+
     public EntityViewModel(VoiceCraftClientEntity entity)
     {
+        _entity = entity;
         _displayName = entity.Name;
         _isMuted = entity.Muted;
         _isDeafened = entity.Deafened;
@@ -23,5 +28,22 @@ public partial class EntityViewModel : ObservableObject
         entity.OnMuteUpdated += (value, _) => IsMuted = value;
         entity.OnDeafenUpdated += (value, _) => IsDeafened = value;
         entity.OnIsVisibleUpdated += (value, _) => IsVisible = value;
+        entity.OnVolumeUpdated += UpdateVolume;
+    }
+
+    private void UpdateVolume(float volume, VoiceCraftEntity entity)
+    {
+        if (_volumeUpdating) return;
+        _volumeUpdating = true;
+        Volume = volume;
+        _volumeUpdating = false;
+    }
+
+    partial void OnVolumeChanging(float value)
+    {
+        if (_volumeUpdating) return;
+        _volumeUpdating = true;
+        _entity.Volume = value;
+        _volumeUpdating = false;
     }
 }
