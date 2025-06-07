6e5c5bb87272bd6f9a29f0195460a59847e187b4
SineVector241 <flyingbirdasuka1@gmail.com>
2025-06-07 12:04:43 +1000

    Finish individual volume per entity.

--- VoiceCraft.Client/VoiceCraft.Client/Views/VoiceView.axaml.cs
+++ VoiceCraft.Client/VoiceCraft.Client/Views/VoiceView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using VoiceCraft.Client.ViewModels;
 
 namespace VoiceCraft.Client.Views;
 
@@ -7,5 +9,14 @@ public partial class VoiceView : UserControl
     public VoiceView()
     {
         InitializeComponent();
+    }
+
+
+    private void ModalBackgroundOnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        var viewModel = (VoiceViewModel?)DataContext;
+        if (viewModel == null) return;
+        if (sender is not Border border || (border.Child?.IsPointerOver ?? false)) return;
+        viewModel.SelectedEntity = null;
     }
 }
