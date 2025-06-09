edfe2eec9a1c83591deb917d3de9df3e32b055b7
SineVector241 <flyingbirdasuka1@gmail.com>
2025-06-09 17:46:53 +1000

    Save entity settings logic.

--- VoiceCraft.Client/VoiceCraft.Client/ViewModels/Data/EntityViewModel.cs
+++ VoiceCraft.Client/VoiceCraft.Client/ViewModels/Data/EntityViewModel.cs
@@ -1,5 +1,8 @@
+using System;
 using CommunityToolkit.Mvvm.ComponentModel;
+using VoiceCraft.Client.Models.Settings;
 using VoiceCraft.Client.Network;
+using VoiceCraft.Client.Services;
 using VoiceCraft.Core;
 
 namespace VoiceCraft.Client.ViewModels.Data;
@@ -13,16 +16,24 @@ public partial class EntityViewModel : ObservableObject
     [ObservableProperty] private float _volume;
 
     private readonly VoiceCraftClientEntity _entity;
+    private readonly EntitySettings _entitySettings;
+    private readonly SettingsService _settingsService;
+    private readonly Guid? _entityUserId;
     private bool _volumeUpdating;
 
-    public EntityViewModel(VoiceCraftClientEntity entity)
+    public EntityViewModel(VoiceCraftClientEntity entity, SettingsService settingsService)
     {
         _entity = entity;
+        _entitySettings = settingsService.EntitySettings;
+        _settingsService = settingsService;
         _displayName = entity.Name;
         _isMuted = entity.Muted;
         _isDeafened = entity.Deafened;
         _isVisible = entity.IsVisible;
         _volume = entity.Volume;
+
+        if (entity is VoiceCraftClientNetworkEntity networkEntity)
+            _entityUserId = networkEntity.UserGuid;
 
         entity.OnNameUpdated += (value, _) => DisplayName = value;
         entity.OnMuteUpdated += (value, _) => IsMuted = value;
@@ -36,6 +47,7 @@ public partial class EntityViewModel : ObservableObject
         if (_volumeUpdating) return;
         _volumeUpdating = true;
         Volume = volume;
+        SaveSettings();
         _volumeUpdating = false;
     }
 
@@ -45,5 +57,19 @@ public partial class EntityViewModel : ObservableObject
         _volumeUpdating = true;
         _entity.Volume = value;
         _volumeUpdating = false;
+    }
+
+    private void SaveSettings()
+    {
+        if (_entityUserId == null) return;
+        if (!_entitySettings.Entities.TryGetValue((Guid)_entityUserId, out var entity))
+        {
+            entity = new EntitySetting();
+            _entitySettings.Entities.Add((Guid)_entityUserId, entity);
+        }
+
+        entity.Volume = Volume;
+
+        _ = _settingsService.SaveAsync();
     }
 }
