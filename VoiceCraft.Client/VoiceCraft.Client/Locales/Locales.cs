1dbfde6f60b3a0b2b1e9d921a97315c1729230d0
SineVector241 <flyingbirdasuka1@gmail.com>
2025-06-22 17:45:51 +1000

    Update unny's icon. Made credits view properly render for small screen sizes.

--- VoiceCraft.Client/VoiceCraft.Client/Locales/Locales.cs
+++ VoiceCraft.Client/VoiceCraft.Client/Locales/Locales.cs
@@ -12,6 +12,11 @@ public static class Locales
     public static string Notification_Badges_VoiceCraft => Localizer.Get("Notification.Badges.VoiceCraft");
     public static string Notification_Badges_CrashLogs => Localizer.Get("Notification.Badges.CrashLogs");
     
+    public static string Credits_AppVersion => Localizer.Get("Credits.AppVersion");
+    public static string Credits_Version => Localizer.Get("Credits.Version");
+    public static string Credits_Codec => Localizer.Get("Credits.Codec");
+    
+    
     public static string SelectedServer_ServerInfo_Status_Pinging => Localizer.Get("SelectedServer.ServerInfo.Status.Pinging");
     public static string SelectedServer_ServerInfo_Status_Latency => Localizer.Get("SelectedServer.ServerInfo.Status.Latency");
     public static string SelectedServer_ServerInfo_Status_Motd => Localizer.Get("SelectedServer.ServerInfo.Status.Motd");
