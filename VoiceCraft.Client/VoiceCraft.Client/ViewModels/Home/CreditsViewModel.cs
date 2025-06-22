28c78e75284f97037494e3a94c1f09d318239afd
SineVector241 <flyingbirdasuka1@gmail.com>
2025-06-22 17:45:51 +1000

    Update unny's icon. Made credits view properly render for small screen sizes.

--- VoiceCraft.Client/VoiceCraft.Client/ViewModels/Home/CreditsViewModel.cs
+++ VoiceCraft.Client/VoiceCraft.Client/ViewModels/Home/CreditsViewModel.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Jeek.Avalonia.Localization;
 using OpusSharp.Core;
 using VoiceCraft.Client.Models;
 using VoiceCraft.Client.Network;
@@ -12,13 +13,13 @@ namespace VoiceCraft.Client.ViewModels.Home;
 
 public partial class CreditsViewModel : ViewModelBase
 {
-    private readonly Bitmap? _defaultIcon = LoadImage("avares://VoiceCraft.Client/Assets/vcLow.png");
+    //private readonly Bitmap? _defaultIcon = LoadImage("avares://VoiceCraft.Client/Assets/Contributors/vc.png");
 
-    [ObservableProperty] private string _appVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "N.A.";
+    [ObservableProperty] private string _appVersion = string.Empty;
+    
+    [ObservableProperty] private string _version = string.Empty;
 
-    [ObservableProperty] private string _opusVersion = OpusInfo.Version();
-
-    [ObservableProperty] private Version _voicecraftVersion = VoiceCraftClient.Version;
+    [ObservableProperty] private string _codec = string.Empty;
 
     [ObservableProperty] private ObservableCollection<Contributor> _contributors;
 
@@ -29,20 +30,30 @@ public partial class CreditsViewModel : ViewModelBase
             new Contributor(
                 "SineVector241",
                 ["Credits.Roles.Author", "Credits.Roles.Programmer"],
-                LoadImage("avares://VoiceCraft.Client/Assets/sinePlushie.png")),
+                LoadImage("avares://VoiceCraft.Client/Assets/Contributors/sinePlushie.png")),
             new Contributor(
                 "Miniontoby",
                 ["Credits.Roles.Translator", "Credits.Roles.Programmer"],
-                LoadImage("avares://VoiceCraft.Client/Assets/minionToby.png")),
+                LoadImage("avares://VoiceCraft.Client/Assets/Contributors/minionToby.png")),
             new Contributor(
                 "Unny",
                 ["Credits.Roles.Translator"],
-                _defaultIcon)
+                LoadImage("avares://VoiceCraft.Client/Assets/Contributors/unny.png"))
         ];
+        
+        Localizer.LanguageChanged += (_, _) => UpdateLocalizations();
+        UpdateLocalizations();
     }
 
     private static Bitmap? LoadImage(string path)
     {
         return AssetLoader.Exists(new Uri(path)) ? new Bitmap(AssetLoader.Open(new Uri(path))) : null;
+    }
+    
+    private void UpdateLocalizations()
+    {
+        AppVersion = Locales.Locales.Credits_AppVersion.Replace("{version}", Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "N.A.");
+        Version = Locales.Locales.Credits_Version.Replace("{version}", VoiceCraftClient.Version.ToString());
+        Codec = Locales.Locales.Credits_Codec.Replace("{version}", OpusInfo.Version());
     }
 }
