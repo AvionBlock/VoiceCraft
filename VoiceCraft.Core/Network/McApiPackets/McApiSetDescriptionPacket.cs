d3ac3da090e75e198f21d7d8b0b3f5e33e40e3f2
SineVector241 <flyingbirdasuka1@gmail.com>
2025-07-28 14:52:16 +1000

    Define the ID.

--- VoiceCraft.Core/Network/McApiPackets/McApiSetDescriptionPacket.cs
+++ VoiceCraft.Core/Network/McApiPackets/McApiSetDescriptionPacket.cs
@@ -4,26 +4,30 @@ namespace VoiceCraft.Core.Network.McApiPackets
 {
     public class McApiSetDescriptionPacket : McApiPacket
     {
-        public McApiSetDescriptionPacket(string sessionToken = "", string value = "")
+        public McApiSetDescriptionPacket(string sessionToken = "", int id = 0, string value = "")
         {
             SessionToken = sessionToken;
+            Id = id;
             Value = value;
         }
 
         public override McApiPacketType PacketType => McApiPacketType.SetDescription;
 
         public string SessionToken { get; private set; }
+        public int Id { get; private set; }
         public string Value { get; private set; }
 
         public override void Serialize(NetDataWriter writer)
         {
             writer.Put(SessionToken, Constants.MaxStringLength);
+            writer.Put(Id);
             writer.Put(Value, Constants.MaxStringLength);
         }
 
         public override void Deserialize(NetDataReader reader)
         {
             SessionToken = reader.GetString(Constants.MaxStringLength);
+            Id = reader.GetInt();
             Value = reader.GetString(Constants.MaxStringLength);
         }
     }
