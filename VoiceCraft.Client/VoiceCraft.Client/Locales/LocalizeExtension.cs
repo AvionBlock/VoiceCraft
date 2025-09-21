efb477d6b38e5f8addb5d4f35652baeb0e599019
SineVector241 <flyingbirdasuka1@gmail.com>
2025-09-21 20:08:35 +1000

    Some locales changes. Currently broken but will fix soon.

--- VoiceCraft.Client/VoiceCraft.Client/Locales/LocalizeExtension.cs
+++ VoiceCraft.Client/VoiceCraft.Client/Locales/LocalizeExtension.cs
@@ -11,9 +11,16 @@ public class LocalizeExtension(object arg)
     public object ProvideValue(IServiceProvider serviceProvider)
     {
         if (arg is string key)
-            return new Jeek.Avalonia.Localization.LocalizeExtension(key).ProvideValue(serviceProvider);
+            return new MultiBinding()
+            {
+                Bindings = [new Binding { Source = key }, new Binding { Source = Localizer.Language }],
+                Mode = BindingMode.TwoWay,
+                Converter = new FuncMultiValueConverter<string, string>(x => Localizer.Get(x.ElementAt(0) ?? ""))
+            };
+
         if (arg is not IBinding binding)
             throw new Exception("Argument must be of type IBinding or string!");
+
         var mb = new MultiBinding()
         {
             Bindings = [binding, new Binding { Source = Localizer.Language }],
