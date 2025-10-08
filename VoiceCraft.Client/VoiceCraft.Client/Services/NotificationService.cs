a88040eca5e1471550974acb6890ee482891dc5c
SineVector241 <flyingbirdasuka1@gmail.com>
2025-10-09 10:53:11 +1100

    Change notification system.

--- VoiceCraft.Client/VoiceCraft.Client/Services/NotificationService.cs
+++ VoiceCraft.Client/VoiceCraft.Client/Services/NotificationService.cs
@@ -1,5 +1,4 @@
 using System;
-using Avalonia.Threading;
 using Message.Avalonia;
 using Message.Avalonia.Models;
 
@@ -11,39 +10,30 @@ public class NotificationService(
     public void SendNotification(string message, string? title = null)
     {
         if (settingsService.NotificationSettings.DisableNotifications) return;
-        Dispatcher.UIThread.Invoke(() =>
+        MessageManager.Default.ShowInformationMessage(message, new MessageOptions()
         {
-            MessageManager.Default.ShowInformationMessage(message, new MessageOptions()
-            {
-                Title = title,
-                Duration = TimeSpan.FromMilliseconds(settingsService.NotificationSettings.DismissDelayMs)
-            });
+            Title = title,
+            Duration = TimeSpan.FromMilliseconds(settingsService.NotificationSettings.DismissDelayMs)
         });
     }
 
     public void SendSuccessNotification(string message, string? title = null)
     {
         if (settingsService.NotificationSettings.DisableNotifications) return;
-        Dispatcher.UIThread.Invoke(() =>
+        MessageManager.Default.ShowSuccessMessage(message, new MessageOptions()
         {
-            MessageManager.Default.ShowSuccessMessage(message, new MessageOptions()
-            {
-                Title = title,
-                Duration = TimeSpan.FromMilliseconds(settingsService.NotificationSettings.DismissDelayMs)
-            });
+            Title = title,
+            Duration = TimeSpan.FromMilliseconds(settingsService.NotificationSettings.DismissDelayMs)
         });
     }
 
-    public void SendErrorNotification(string message, string? title = null)
+    public void SendErrorNotification(string message)
     {
         if (settingsService.NotificationSettings.DisableNotifications) return;
-        Dispatcher.UIThread.Invoke(() =>
+        MessageManager.Default.ShowErrorMessage(message, new MessageOptions()
         {
-            MessageManager.Default.ShowErrorMessage(message, new MessageOptions()
-            {
-                Title = title,
-                Duration = TimeSpan.FromMilliseconds(settingsService.NotificationSettings.DismissDelayMs)
-            });
+            Title = Locales.Locales.Notification_Badges_Error,
+            Duration = TimeSpan.FromMilliseconds(settingsService.NotificationSettings.DismissDelayMs)
         });
     }
 }
