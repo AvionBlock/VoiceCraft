46564c0b635dcc2b42770128869ee020646ff084
SineVector241 <flyingbirdasuka1@gmail.com>
2025-12-02 10:08:58 +1100

    Change ping interval to be MC sided.

--- VoiceCraft.Server/Config/McHttpConfig.cs
+++ VoiceCraft.Server/Config/McHttpConfig.cs
@@ -2,7 +2,8 @@ namespace VoiceCraft.Server.Config;
 
 public class McHttpConfig
 {
-    public string LoginToken { get; set; } = string.Empty;
+    public string LoginToken { get; set; } = Guid.NewGuid().ToString();
     public string Hostname { get; set; } = "http://127.0.0.1:9051/";
-    public uint MaxClients { get; set; } = 100;
+    public uint MaxClients { get; set; } = 1;
+    public uint MaxTimeoutMs { get; set; } = 10000;
 }
