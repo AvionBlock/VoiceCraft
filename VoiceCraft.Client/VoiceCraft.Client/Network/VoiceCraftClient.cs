621ac9dabade293f3f31d924c9e6c4c303cf649a
SineVector241 <flyingbirdasuka1@gmail.com>
2025-12-03 11:51:01 +1100

    Stop Command and some locale updates.

--- VoiceCraft.Client/VoiceCraft.Client/Network/VoiceCraftClient.cs
+++ VoiceCraft.Client/VoiceCraft.Client/Network/VoiceCraftClient.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices;
 using LiteNetLib;
--- VoiceCraft.Server/Config/McWssConfig.cs
+++ VoiceCraft.Server/Config/McWssConfig.cs
@@ -2,8 +2,9 @@ namespace VoiceCraft.Server.Config;
 
 public class McWssConfig
 {
+    public bool Enabled { get; set; }
     public string LoginToken { get; set; } = Guid.NewGuid().ToString();
-    public string Hostname { get; set; } = "ws://127.0.0.1:9050/";
+    public string Hostname { get; set; } = "ws://127.0.0.1:9051/";
     public uint MaxClients { get; set; } = 1;
     public uint MaxTimeoutMs { get; set; } = 10000;
     public string TunnelCommand { get; set; } = "voicecraft:data_tunnel";
