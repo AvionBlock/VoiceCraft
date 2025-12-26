d9f1fb9eace63ef21a0096e1e707fb386a768da0
SineVector241 <flyingbirdasuka1@gmail.com>
2025-12-27 10:55:58 +1100

    Handle player teleported event.

--- VoiceCraft.Client/VoiceCraft.Client/Network/McWssServer.cs
+++ VoiceCraft.Client/VoiceCraft.Client/Network/McWssServer.cs
@@ -83,6 +83,7 @@ public class McWssServer(VoiceCraftClient client) : IDisposable
 
         _peerConnection = socket;
         SendEventSubscribe(_peerConnection, "PlayerTravelled");
+        SendEventSubscribe(_peerConnection, "PlayerTeleported");
         _localPlayerRequestId = SendCommand(_peerConnection, "/getlocalplayername");
     }
 
@@ -128,12 +129,44 @@ public class McWssServer(VoiceCraftClient client) : IDisposable
 
     private void HandleEvent(McWssGenericPacket genericPacket, string data)
     {
-        if (genericPacket.header.eventName != "PlayerTravelled") return;
-        var playerTravelledEvent = JsonSerializer.Deserialize<McWssPlayerTravelledEvent>(data);
-        if (playerTravelledEvent == null || playerTravelledEvent.body.player.name != _localPlayerName) return;
+        switch (genericPacket.header.eventName)
+        {
+            case "PlayerTravelled":
+                var playerTravelledEventPacket = JsonSerializer.Deserialize<McWssPlayerTravelledEvent>(data);
+                if(playerTravelledEventPacket == null) return;
+                HandlePlayerTravelledEvent(playerTravelledEventPacket);
+                break;
+            case "PlayerTeleported":
+                var playerTeleportedEvent = JsonSerializer.Deserialize<McWssPlayerTeleportedEvent>(data);
+                if(playerTeleportedEvent == null) return;
+                HandlePlayerTeleportedEvent(playerTeleportedEvent);
+                break;
+        }
+    }
+
+    private void HandlePlayerTravelledEvent(McWssPlayerTravelledEvent playerTravelledEvent)
+    {
+        if (playerTravelledEvent.body.player.name != _localPlayerName) return;
         var position = playerTravelledEvent.body.player.position;
         var rotation = playerTravelledEvent.body.player.yRot;
         var dimensionId = playerTravelledEvent.body.player.dimension;
+        _client.Position = new Vector3(position.x, position.y, position.z);
+        _client.Rotation = new Vector2(0, rotation);
+        _client.WorldId = dimensionId switch
+        {
+            0 => "minecraft:overworld",
+            1 => "minecraft:nether",
+            2 => "minecraft:end",
+            _ => $"{dimensionId}"
+        };
+    }
+
+    private void HandlePlayerTeleportedEvent(McWssPlayerTeleportedEvent playerTeleportedEvent)
+    {
+        if (playerTeleportedEvent.body.player.name != _localPlayerName) return;
+        var position = playerTeleportedEvent.body.player.position;
+        var rotation = playerTeleportedEvent.body.player.yRot;
+        var dimensionId = playerTeleportedEvent.body.player.dimension;
         _client.Position = new Vector3(position.x, position.y, position.z);
         _client.Rotation = new Vector2(0, rotation);
         _client.WorldId = dimensionId switch
