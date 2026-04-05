2ef3b8b6caa371a41a6fa248dd4ff3634ed4f2dd
abubachir122 <225525001+lil-jon-crunk@users.noreply.github.com>
2026-04-05 17:30:40 +0300

    Use system UI culture as default locale

--- VoiceCraft.Client/VoiceCraft.Client/Models/Settings/LocaleSettings.cs
+++ VoiceCraft.Client/VoiceCraft.Client/Models/Settings/LocaleSettings.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using VoiceCraft.Client.Services;
 using VoiceCraft.Core;
 using VoiceCraft.Core.Locales;
@@ -8,9 +10,7 @@ namespace VoiceCraft.Client.Models.Settings;
 
 public class LocaleSettings : Setting<LocaleSettings>
 {
-    private string _culture = Localizer.Languages.Contains(CultureInfo.CurrentCulture.Name)
-        ? CultureInfo.CurrentCulture.Name
-        : Constants.DefaultLanguage;
+    private string _culture = ResolvePreferredCulture();
 
     public string Culture
     {
@@ -24,10 +24,57 @@ public class LocaleSettings : Setting<LocaleSettings>
 
     public override event Action<LocaleSettings>? OnUpdated;
 
+    public override bool OnLoading()
+    {
+        if (!Localizer.Languages.Contains(_culture))
+            _culture = ResolvePreferredCulture();
+        return base.OnLoading();
+    }
+
     public override object Clone()
     {
         var clone = (LocaleSettings)MemberwiseClone();
         clone.OnUpdated = null;
         return clone;
+    }
+
+    private static string ResolvePreferredCulture()
+    {
+        return ResolvePreferredCulture(CultureInfo.CurrentUICulture.Name, Localizer.Languages);
+    }
+
+    private static string ResolvePreferredCulture(string currentCulture, IEnumerable<string> supportedLanguages)
+    {
+        var supported = supportedLanguages.ToArray();
+        if (supported.Length == 0)
+            return Constants.DefaultLanguage;
+
+        var exact = supported.FirstOrDefault(x => x.Equals(currentCulture, StringComparison.OrdinalIgnoreCase));
+        if (exact != null)
+            return exact;
+
+        var language = TryGetLanguageCode(currentCulture);
+        if (!string.IsNullOrWhiteSpace(language))
+        {
+            var neutral = supported.FirstOrDefault(x => x.Equals(language, StringComparison.OrdinalIgnoreCase));
+            if (neutral != null)
+                return neutral;
+
+            var regional = supported.FirstOrDefault(x =>
+                x.StartsWith(language + "-", StringComparison.OrdinalIgnoreCase));
+            if (regional != null)
+                return regional;
+        }
+
+        return supported.FirstOrDefault(x => x.Equals(Constants.DefaultLanguage, StringComparison.OrdinalIgnoreCase))
+               ?? supported[0];
+    }
+
+    private static string TryGetLanguageCode(string cultureName)
+    {
+        if (string.IsNullOrWhiteSpace(cultureName))
+            return string.Empty;
+        var separator = cultureName.IndexOf('-');
+        return separator == -1 ? cultureName : cultureName[..separator];
     }
 }
