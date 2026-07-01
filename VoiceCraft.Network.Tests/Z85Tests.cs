65aa9a6d167582141f7954359241240f9b0c0202
SineVector241 <flyingbirdasuka1@gmail.com>
2026-07-01 16:57:03 +1000

    Revert Z85 implementation, too hard to port to JS since JS has no pointers or uint specific types.

--- VoiceCraft.Network.Tests/Z85Tests.cs
+++ VoiceCraft.Network.Tests/Z85Tests.cs
@@ -1,9 +1,19 @@
+using LiteNetLib.Utils;
+using VoiceCraft.Network.Packets.McApiPackets.Request;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace VoiceCraft.Network.Tests;
 
 public class Z85Tests
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Z85Tests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Fact]
     public void GetString_WithUnpaddedLength_ThrowsArgumentOutOfRangeException()
     {
@@ -26,5 +36,20 @@ public class Z85Tests
         var encoded = Z85.GetStringWithPadding(bytes);
         var decoded = Z85.GetBytesWithPadding(encoded);
         Assert.Equal(bytes, decoded);
+    }
+
+    [Fact]
+    public void LoginPacketTest()
+    {
+        var packet = new McApiLoginRequestPacket("testAAA", "test2AAAA", new Version(1, 7, 0), []);
+        var writer = new NetDataWriter();
+        var reader = new NetDataReader();
+        packet.Serialize(writer);
+        var encoded = writer.CopyData();
+        var z85Encoded = Z85.GetStringWithPadding(encoded);
+        var z85Decoded = Z85.GetBytesWithPadding(z85Encoded);
+        reader.SetSource(z85Decoded);
+        packet.Deserialize(reader);
+        _testOutputHelper.WriteLine(z85Encoded);
     }
 }
